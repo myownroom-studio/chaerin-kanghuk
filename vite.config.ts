c6f9cc13677313a87ed098d9c5b20e64c4ffb056
Miran Lee <ranz_@naver.com>
2025-01-13 00:46:37 +0900

    add new module files and update package.json dependencies
    - 개발/운영 환경 별로 다른 경로를 사용해야 하는 이슈 해결

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,26 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: process.env.VITE_BASE_URL,
-  plugins: [react(), tsconfigPaths(), svgr()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    base: env.VITE_BASE_URL,
+    plugins: [
+      react(),
+      tsconfigPaths(),
+      svgr(),
+      createHtmlPlugin({
+        inject: {
+          data: {
+            VITE_BASE_URL: env.VITE_BASE_URL,
+          },
+        },
+      }),
+    ],
+  };
 });
