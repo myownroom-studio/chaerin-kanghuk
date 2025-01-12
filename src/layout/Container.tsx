84ddc31128f0d7502da2e3495a41f4b503f26890
Miran Lee <ranz_@naver.com>
2025-01-12 23:50:42 +0900

    background issue fixed

--- src/layout/Container.tsx
+++ src/layout/Container.tsx
@@ -1,8 +1,10 @@
 import styled from '@emotion/styled';
 
+const baseUrl = import.meta.env.VITE_BASE_URL;
+
 const Container = styled.div`
   border: 30px solid transparent; /* 테두리의 초기 값 설정 */
-  border-image-source: url('/background.png'); /* 이미지 경로 설정 */
+  border-image-source: url('${baseUrl}background.png'); /* 이미지 경로 설정 */
   border-image-slice: 30% 49%; /* 이미지의 크기 설정 */
   border-image-width: 280px; /* 테두리 이미지의 너비 설정 */
   background-color: #ffffff;
