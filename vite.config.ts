91869e7e87bd5bc1c4a080388b9f679be81dad65
Miran Lee <ranz_@naver.com>
2025-01-12 21:03:29 +0900

    fix cache issue and update .gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/chaerin-kanghuk',
+  base: '/chaerin-kanghuk/',
   plugins: [react(), tsconfigPaths(), svgr()],
 });
